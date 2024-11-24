9c181dcc333a90a40098223ee5ad66787216aa4f
Victorriv <Victorrivera714@yahoo.com>
2024-11-24 15:42:54 -0800

    practicing if age

---
+++ index.js
@@ -0,0 +1,9 @@
+// IF STATEMENTS = if a condition is true, execute some code if not, do something
+
+
+let age = 25; 
+
+if(age >= 18){
+    console.log ("you are old enough to enter the site");
+
+}
